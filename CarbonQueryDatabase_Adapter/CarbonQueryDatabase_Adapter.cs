f19c113f042d64e1dfbf96e5530b574c70d9f28a
Michael Hoehn <michael.hoehn@burohappold.com>
2020-01-15 15:48:11 -0500

    Creation of initial adapter and pull action

---
+++ CarbonQueryDatabase_Adapter/CarbonQueryDatabase_Adapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.Adapter;
+using System.Net;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Adapter.CarbonQueryDatabase
+{
+    public partial class CarbonQueryDatabaseAdapter : BHoMAdapter
+    {
+        /***************************************************/
+        /**** Public fields                             ****/
+        /***************************************************/
+
+        public static string AdapterID = "CarbonQueryDatabaseAdapter";
+
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        [Description("Description goes here. Description goes here.")]
+        [Input("username", "Provide EC3 Username")]
+        [Input("password", "Provide EC3 Password")]
+        [Output("adapter", "adapter results")]
+
+        public CarbonQueryDatabaseAdapter(string username = "", string password = "")
+        {
+
+        System.Net.ServicePointManager.SecurityProtocol =
+                SecurityProtocolType.Ssl3 |
+                SecurityProtocolType.Tls12 |
+                SecurityProtocolType.Tls11 |
+                SecurityProtocolType.Tls;
+        }
+    }
+}
---
+++ LifeCycleAnalysis_Engine/Create/CarbonQueryDatabaseRequest.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System.Collections.Generic;
+using BH.oM.LifeCycleAnalysis;
+
+namespace BH.Engine.LifeCycleAnalysis
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public  Method                            ****/
+        /***************************************************/
+
+        /***************************************************/
+
+        public static CarbonQueryDatabaseRequest CarbonQueryDatabaseRequest(string uri, string username)
+        {
+            return new CarbonQueryDatabaseRequest
+            {
+                Uri = "-H " + "Authorization: Bearer " + username + uri,
+            };
+        }
+
+        /***************************************************/
+
+        public static CarbonQueryDatabaseRequest CarbonQueryDatabaseRequest0(string uri, string username, string password)
+        {
+            return new CarbonQueryDatabaseRequest
+            {
+                Uri = "curl - X POST " + "https://etl-api.cqd.io/api/rest-auth/login" + " - H " + "accept: application/json" + " - H " + "Content - Type: application / json" + " - d " + "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}",
+            };
+        }
+
+        /***************************************************/
+
+        public static CarbonQueryDatabaseRequest CarbonQueryDatabaseRequest(string uri, CustomObject headers = null, CustomObject parameters = null)
+        {
+            return new CarbonQueryDatabaseRequest
+            {
+                Uri = uri,
+                Headers = headers?.CustomData,
+                Parameters = parameters?.CustomData
+            };
+        }
+
+        /***************************************************/
+    }
+}
