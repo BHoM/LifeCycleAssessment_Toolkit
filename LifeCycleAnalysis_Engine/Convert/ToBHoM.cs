0b699c7d228aa4e674b92f2089ba8eb2e5b42945
Erik Narhi <Erik.Narhi@burohappold.com>
2020-01-30 13:03:17 -0800

    Convert method and EC3 object created

---
+++ LifeCycleAnalysis_Engine/Convert/ToBHoM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+using BH.oM.LifeCycleAnalysis;
+using BH.Engine.Reflection;
+
+namespace BH.Engine.LifeCycleAnalysis
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /****           Public Methods                  ****/
+        /***************************************************/
+
+        public static EC3Data ToBHoMObject(this CustomObject obj)
+        {
+            int result = 0;
+
+            EC3Data data = new EC3Data
+            {
+                id = obj.PropertyValue("id")?.ToString() ?? "",
+
+                name = obj.PropertyValue("name")?.ToString() ?? "",
+
+                manufacturer = obj.PropertyValue("manufacturer.name")?.ToString() ?? "",
+
+                plant = obj.PropertyValue("plant.name")?.ToString() ?? "",
+
+                postalCode = int.TryParse(obj.PropertyValue("plant.postal_code").ToString(), out result) ? result : 0,
+
+                density = obj.PropertyValue("density")?.ToString() ?? "",
+
+                gwpPerKG = obj.PropertyValue("gwp_per_kg")?.ToString() ?? "",
+            };
+                      
+            
+            return data;
+        }
+    }
+}
