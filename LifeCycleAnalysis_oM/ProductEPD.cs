1def260df1dff6ccca743c72893fcfbbf45d1fe8
Erik Narhi <Erik.Narhi@burohappold.com>
2020-02-20 15:19:32 -0800

    EPD added and redundant convert removed

---
+++ LifeCycleAnalysis_oM/ProductEPD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.oM.LifeCycleAnalysis
+{
+    public class SectorEPD : EPDData
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Manufacturer { get; set; } = "";
+        public string Plant { get; set; } = "";
+        public int PostalCode { get; set; } = 0;
+        public List<string> IndustryStandards { get; set; } = new List<string>(); //returns blank lists
+
+        /***************************************************/
+    }
+}
---
+++ LifeCycleAnalysis_oM/SectorEPD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.oM.LifeCycleAnalysis
+{
+    public class ProductEPD : EPDData
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Publisher { get; set; } = "";
+        public string Jurisdiction { get; set; } = "";
+
+        /***************************************************/
+    }
+}
