8e4da30b704e5f64ee2020e5e278e4e65459e57a
Michael Hoehn <michael.hoehn@burohappold.com>
2020-02-20 10:45:39 -0500

    Save point

---
+++ LifeCycleAnalysis_Engine/Convert/ToBHoM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+using BH.oM.LifeCycleAnalysis;
+using BH.Engine.Reflection;
+
+namespace BH.Engine.LifeCycleAnalysis
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /****           Public Methods                  ****/
+        /***************************************************/
+
+        public static EPDData ToBHoMObject(this CustomObject obj)
+        {
+            int result = 0;
+
+            List<string> standards = new List<string>();
+            if (obj.PropertyValue("industry_standards") != null)
+                standards = obj.PropertyValue("industry_standards") as List<string>;
+
+            EPDData data = new EPDData
+            {
+                Id = obj.PropertyValue("id")?.ToString() ?? "",
+
+                Name = obj.PropertyValue("name")?.ToString() ?? "",
+
+                Manufacturer = obj.PropertyValue("manufacturer.name")?.ToString() ?? "",
+
+                Plant = obj.PropertyValue("plant.name")?.ToString() ?? "",
+
+                PostalCode = int.TryParse(obj.PropertyValue("plant.postal_code")?.ToString() ?? "", out result) ? result : 0,
+
+                Density = obj.PropertyValue("density")?.ToString() ?? "",
+
+                GwpPerKG = obj.PropertyValue("gwp_per_kg")?.ToString() ?? "",
+
+                BiogenicEmbodiedCarbon = obj.PropertyValue("biogenic_embodied_carbon_z") != null ? System.Convert.ToDouble(obj.PropertyValue("biogenic_embodied_carbon_z")) : double.NaN,
+
+                DeclaredUnit = obj.PropertyValue("declared_unit")?.ToString() ?? "",
+
+                Description = obj.PropertyValue("description")?.ToString() ?? "",
+
+                IndustryStandards = standards,
+            };
+            return data;
+        }
+    }
+}
---
+++ LifeCycleAnalysis_oM/EPDData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.oM.LifeCycleAnalysis
+{
+    public class EPDData : BHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Manufacturer { get; set; } = "";
+        public string Plant { get; set; } = "";
+        public int PostalCode { get; set; } = 0;
+        public string Density { get; set; } = "";
+        public string GwpPerKG { get; set; } = "";
+        public double BiogenicEmbodiedCarbon { get; set; } = double.NaN;
+        public string DeclaredUnit { get; set; } = "";
+        public string Description { get; set; } = "";
+        public List<string> IndustryStandards { get; set; } = new List<string>();
+
+        /***************************************************/
+    }
+}
