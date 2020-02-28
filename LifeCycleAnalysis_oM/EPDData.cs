50c8f16cd76f2881b55ae0abf8832f00f69309a4
Michael Hoehn <michael.hoehn@burohappold.com>
2020-02-28 18:05:27 -0500

    Adding object properties from Quartz database

--- LifeCycleAnalysis_oM/EPDData.cs
+++ LifeCycleAnalysis_oM/EPDData.cs
@@ -16,6 +16,20 @@ namespace BH.oM.LifeCycleAnalysis
         public double BiogenicEmbodiedCarbon { get; set; } = double.NaN;
         public string DeclaredUnit { get; set; } = "";
         public string Description { get; set; } = "";
+        public string EPDScope { get; set; } = "";
+        public double GlobalWarmingPotential { get; set; } = double.NaN;
+        public double OzoneDepletionPotential { get; set; } = double.NaN;
+        public double PhotochemicalOzoneCreationPotential { get; set; } = double.NaN;
+        public double AcidificationPotential { get; set; } = double.NaN;
+        public double EutrophicationPotential { get; set; } = double.NaN;
+        public double DepletionOfAbioticResourcesFossilFuels { get; set; } = double.NaN;
+        public double GlobalWarmingPotentialEOL { get; set; }
+        public double OzoneDepletionPotentialEOL { get; set; }
+        public double PhotochemicalOzoneCreationPotentialEOL { get; set; }
+        public double AcidificationPotentialEOL { get; set; }
+        public double EutrophicationPotentialEOL { get; set; }
+        public double DepletionOfAbioticResourcesFossilFuelsEOL { get; set; }
+        public string TreatmentEOL { get; set; }
 
         /***************************************************/
     }
