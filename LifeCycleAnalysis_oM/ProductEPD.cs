6086abc0c96f6b2beb4e0b4ac1af08877312274d
Erik Narhi <Erik.Narhi@burohappold.com>
2020-03-02 16:41:41 -0800

    Set property defaults

--- LifeCycleAnalysis_oM/ProductEPD.cs
+++ LifeCycleAnalysis_oM/ProductEPD.cs
@@ -12,7 +12,7 @@ namespace BH.oM.LifeCycleAnalysis
         public string Manufacturer { get; set; } = "";
         public string Plant { get; set; } = "";
         public int PostalCode { get; set; } = 0;
-        public List<string> IndustryStandards { get; set; } = new List<string>(); //returns blank lists
+        public List<string> IndustryStandards { get; set; } = new List<string>();
 
         /***************************************************/
     }
