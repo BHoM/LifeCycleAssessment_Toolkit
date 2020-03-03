db20a159777f4b7beb08f93f3c6c2ff799c38f65
Erik Narhi <Erik.Narhi@burohappold.com>
2020-03-02 17:00:19 -0800

    Implement EPD Interface

---
+++ LifeCycleAnalysis_oM/IEPDData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using BH.oM.Base;
+
+namespace BH.oM.LifeCycleAnalysis
+{
+    public interface IEPDData : IBHoMObject
+    {
+        /***************************************************/
+        /**** Properties                                ****/
+        /***************************************************/
+
+        string Id { get; set; }
+        string Density { get; set; }
+        string GwpPerKG { get; set; }
+        string GwpPerDeclaredUnit { get; set; }
+        double BiogenicEmbodiedCarbon { get; set; }
+        string DeclaredUnit { get; set; }
+        string Description { get; set; }
+        string EPDScope { get; set; }
+        double GlobalWarmingPotential { get; set; }
+        double OzoneDepletionPotential { get; set; }
+        double PhotochemicalOzoneCreationPotential { get; set; }
+        double AcidificationPotential { get; set; }
+        double EutrophicationPotential { get; set; }
+        double DepletionOfAbioticResourcesFossilFuels { get; set; }
+        double GlobalWarmingPotentialEOL { get; set; }
+        double OzoneDepletionPotentialEOL { get; set; }
+        double PhotochemicalOzoneCreationPotentialEOL { get; set; }
+        double AcidificationPotentialEOL { get; set; }
+        double EutrophicationPotentialEOL { get; set; }
+        double DepletionOfAbioticResourcesFossilFuelsEOL { get; set; }
+        string TreatmentEOL { get; set; }
+
+        /***************************************************/
+    }
+}
--- LifeCycleAnalysis_oM/SectorEPD.cs
+++ LifeCycleAnalysis_oM/SectorEPD.cs
@@ -3,7 +3,7 @@ using BH.oM.Base;
 
 namespace BH.oM.LifeCycleAnalysis
 {
-    public class SectorEPD : EPDData
+    public class SectorEPD : BHoMObject, IEPDData
     {
         /***************************************************/
         /**** Properties                                ****/
@@ -11,6 +11,27 @@ namespace BH.oM.LifeCycleAnalysis
 
         public List<string> Publisher { get; set; } = new List<string>();
         public List<string> Jurisdiction { get; set; } = new List<string>();
+        public string Id { get; set; } = "";
+        public string Density { get; set; } = "";
+        public string GwpPerKG { get; set; } = "";
+        public string GwpPerDeclaredUnit { get; set; } = "";
+        public double BiogenicEmbodiedCarbon { get; set; } = double.NaN;
+        public string DeclaredUnit { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string EPDScope { get; set; } = "";
+        public double GlobalWarmingPotential { get; set; } = double.NaN;
+        public double OzoneDepletionPotential { get; set; } = double.NaN;
+        public double PhotochemicalOzoneCreationPotential { get; set; } = double.NaN;
+        public double AcidificationPotential { get; set; } = double.NaN;
+        public double EutrophicationPotential { get; set; } = double.NaN;
+        public double DepletionOfAbioticResourcesFossilFuels { get; set; } = double.NaN;
+        public double GlobalWarmingPotentialEOL { get; set; } = double.NaN;
+        public double OzoneDepletionPotentialEOL { get; set; } = double.NaN;
+        public double PhotochemicalOzoneCreationPotentialEOL { get; set; } = double.NaN;
+        public double AcidificationPotentialEOL { get; set; } = double.NaN;
+        public double EutrophicationPotentialEOL { get; set; } = double.NaN;
+        public double DepletionOfAbioticResourcesFossilFuelsEOL { get; set; } = double.NaN;
+        public string TreatmentEOL { get; set; } = "";
 
         /***************************************************/
     }
