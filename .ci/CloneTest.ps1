ccd7f7d6ff25038549b8b9ad39a1488c28caea48
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-03-11 11:05:37 +0000

    Update CI files

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -13,17 +13,3 @@ git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURC
 # **** Restore NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
-
-# **** Cloning BuroHappold_Test_Toolkit ****
-$repo = "BuroHappold_Test_Toolkit"
-
-# **** Defining Paths ****
-$slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
-
-# **** Cloning Repo ****
-Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
-git clone -q --branch=master https://pat:$env:BHOM_ACCESSTOKEN@github.com/BuroHappoldEngineering/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
-
-# **** Restore NuGet ****
-Write-Output ("Restoring NuGet packages for " + $repo)
-& NuGet.exe restore $slnPath
