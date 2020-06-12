17587d1386660ca8cfa4c8fc601d3ac211ab36a1
Michael Hoehn <michael.hoehn@burohappold.com>
2020-06-12 16:59:50 -0400

    Eval methods with results

---
+++ LifeCycleAssessment_Engine/Query/TotalGlobalWarmingPotential.cs
@@ -0,0 +1,24 @@
+﻿using BH.oM.LifeCycleAssessment;
+using BH.oM.LifeCycleAssessment.Results;
+using BH.oM.Reflection.Attributes;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.LifeCycleAssessment
+{
+    public static partial class Query
+    {
+        [Description("Query the total global warming potential, or GWP from any LifeCycleAssessmentElementResult data. This is typically evaluated by kgCO2e or kg of Carbon Dioxide equivalent.")]
+        [Input("results", "Supply a valid GlobalWarmingPotentialResult from a scope evaluation.")]
+        [Output("gwp", "The combined amount of kgCO2e of the objects provided.")]
+        public static double TotalGlobalWarmingPotential(this List<LifeCycleAssessmentElementResult> gwpResults)
+        {
+            return gwpResults.Where(x => x is GlobalWarmingPotentialResult).Select(x => (x as GlobalWarmingPotentialResult).GlobalWarmingPotential).Sum();
+        }
+    }
+}
