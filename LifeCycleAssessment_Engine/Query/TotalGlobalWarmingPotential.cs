bba263ddce4456ff8bd8d082a690aea73519a294
Michael Hoehn <michael.hoehn@burohappold.com>
2020-06-12 17:16:55 -0400

    Compliance

--- LifeCycleAssessment_Engine/Query/TotalGlobalWarmingPotential.cs
+++ LifeCycleAssessment_Engine/Query/TotalGlobalWarmingPotential.cs
@@ -1,4 +1,26 @@
-﻿using BH.oM.LifeCycleAssessment;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.LifeCycleAssessment;
 using BH.oM.LifeCycleAssessment.Results;
 using BH.oM.Reflection.Attributes;
 using System;
@@ -14,7 +36,7 @@ namespace BH.Engine.LifeCycleAssessment
     public static partial class Query
     {
         [Description("Query the total global warming potential, or GWP from any LifeCycleAssessmentElementResult data. This is typically evaluated by kgCO2e or kg of Carbon Dioxide equivalent.")]
-        [Input("results", "Supply a valid GlobalWarmingPotentialResult from a scope evaluation.")]
+        [Input("gwpResults", "Supply a valid GlobalWarmingPotentialResult from a scope evaluation.")]
         [Output("gwp", "The combined amount of kgCO2e of the objects provided.")]
         public static double TotalGlobalWarmingPotential(this List<LifeCycleAssessmentElementResult> gwpResults)
         {
