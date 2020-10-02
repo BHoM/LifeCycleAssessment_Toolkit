c214a9d54a1658171a2acb8b151a9791ec6514f4
Michael Hoehn <michael.hoehn@burohappold.com>
2020-10-02 18:08:02 -0400

    Added datasets

--- LifeCycleAssessment_Engine/Query/EnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_Engine/Query/EnvironmentalProductDeclaration.cs
@@ -50,18 +50,6 @@ namespace BH.Engine.LifeCycleAssessment
                     return epd.EutrophicationPotential;
                 case EnvironmentalProductDeclarationField.DepletionOfAbioticResourcesFossilFuels:
                     return epd.DepletionOfAbioticResourcesFossilFuels;
-                case EnvironmentalProductDeclarationField.GlobalWarmingPotentialEndOfLife:
-                    return epd.GlobalWarmingPotentialEndOfLife;
-                case EnvironmentalProductDeclarationField.OzoneDepletionPotentialEndOfLife:
-                    return epd.OzoneDepletionPotentialEndOfLife;
-                case EnvironmentalProductDeclarationField.PhotochemicalOzoneCreationPotentialEndOfLife:
-                    return epd.PhotochemicalOzoneCreationPotentialEndOfLife;
-                case EnvironmentalProductDeclarationField.AcidificationPotentialEndOfLife:
-                    return epd.AcidificationPotentialEndOfLife;
-                case EnvironmentalProductDeclarationField.EutrophicationPotentialEndOfLife:
-                    return epd.EutrophicationPotentialEndOfLife;
-                case EnvironmentalProductDeclarationField.DepletionOfAbioticResourcesFossilFuelsEndOfLife:
-                    return epd.DepletionOfAbioticResourcesFossilFuelsEndOfLife;
                 default:
                     return 0;
             }
