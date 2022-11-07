7cae11480f0b128bcdd65d4f756d6edf964c1194
Isak Naslund <Isak.Naslund@burohappold.com>
2022-11-07 12:50:54 +0100

    Adding new method for evaluating EPD returning new ResultClasses
    Suffix 2 added to the method instead of replacing to compare the two. If accepted as new method, should remove the old and suffix with it.

--- LifeCycleAssessment_Engine/Compute/EvaluateEnvironmentalProductDeclaration.cs
+++ LifeCycleAssessment_Engine/Compute/EvaluateEnvironmentalProductDeclaration.cs
@@ -30,6 +30,11 @@ using System.Linq;
 using BH.oM.Physical.Materials;
 using BH.Engine.Matter;
 using BH.Engine.LifeCycleAssessment.Objects;
+using BH.oM.LifeCycleAssessment.MaterialFragments;
+using BH.Engine.Spatial;
+using BH.oM.Base;
+using BH.oM.Quantities.Attributes;
+using System.Xml.Linq;
 
 namespace BH.Engine.LifeCycleAssessment
 {
@@ -121,6 +126,142 @@ namespace BH.Engine.LifeCycleAssessment
         /***************************************************/
 
 
+        [Description("This method calculates the results of any selected metric within an Environmental Product Declaration. For example for an EPD of QuantityType Volume, results will reflect the objects volume * EPD Field metric.")]
+        [Input("elementM", "This is a BHoM object used to calculate EPD metric. This obj must have an EPD MaterialFragment applied to the object.")]
+        [Input("phases", "Provide phases of life you wish to evaluate. Phases of life must be documented within EPDs for this method to work.")]
+        [Input("field", "Filter the provided EnvironmentalProductDeclaration by selecting one of the provided metrics for calculation. This method also accepts multiple fields simultaneously.")]
+        [Input("exactMatch", "If true, the evaluation method will force an exact LCA phase match to solve for.")]
+        [Output("result", "A LifeCycleElementResult that contains the LifeCycleAssessment data for the input object.")]
+        public static ElementResult EvaluateEnvironmentalProductDeclaration2(IElementM elementM, List<LifeCycleAssessmentPhases> phases, EnvironmentalProductDeclarationField field = EnvironmentalProductDeclarationField.GlobalWarmingPotential, bool exactMatch = false, List<Material> templateMaterials = null, bool prioritiseTemplate = true)
+        {
+            if (elementM == null)
+            {
+
+                return null;
+            }
+
+            MaterialComposition mc = elementM.MappedMaterialComposition(templateMaterials, true, prioritiseTemplate);
+
+            if (mc == null)
+            {
+                return null;
+            }
+
+            if (mc.Materials.Count == 0)
+                return null;
+
+            //Predefining parameters to be used for some item types in loop
+            double totalVolume = double.NaN;
+            double area = double.NaN;
+            double length = double.NaN;
+
+            List<MaterialResult> materialResults = new List<MaterialResult>();
+
+            for (int i = 0; i < mc.Materials.Count; i++)
+            {
+                Material material = mc.Materials[i];
+                EnvironmentalProductDeclaration epd = material.Properties.OfType<EnvironmentalProductDeclaration>().FirstOrDefault();
+
+                if (epd == null)
+                {
+                    Engine.Base.Compute.RecordError($"EPD not set to material {material.Name}. Unable to evaluate {field}.");
+                    return null;
+                }
+
+                double evaluationValue = epd.GetEvaluationValue(field, phases, exactMatch);
+                if (double.IsNaN(evaluationValue))
+                {
+                    Base.Compute.RecordError($"EPD {epd.Name} does not contain {field} for phases {string.Join(",", phases)}.");
+                    return null;
+                }
+
+                double quantityValue;
+                switch (epd.QuantityType)
+                {
+                    case QuantityType.Volume:
+                        if (double.IsNaN(totalVolume))
+                            totalVolume = elementM.ISolidVolume();
+                        quantityValue = totalVolume * mc.Ratios[i];
+                        break;
+                    case QuantityType.Area:
+                        if (double.IsNaN(area))
+                        {
+                            IElement2D element2D = elementM as IElement2D;
+                            if (element2D == null)
+                            {
+                                Base.Compute.RecordError($"Can only evaluate Area based epds on elements of a {nameof(IElement2D)} type.");
+                                return null;
+                            }
+                            area = element2D.Area();
+                        }
+                        quantityValue = area;
+                        break;
+                    case QuantityType.Length:
+                        if (double.IsNaN(length))
+                        {
+                            IElement1D element1D = elementM as IElement1D;
+                            if (element1D == null)
+                            {
+                                Base.Compute.RecordError($"Can only evaluate Area based epds on elements of a {nameof(IElement1D)} type.");
+                                return null;
+                            }
+                            length = element1D.Length();
+                        }
+                        quantityValue = length;
+                        break;
+                    case QuantityType.Mass:
+                        if (double.IsNaN(totalVolume))
+                            totalVolume = elementM.ISolidVolume();
+                        double materialVolume = totalVolume * mc.Ratios[i];
+
+                        if (double.IsNaN(material.Density))
+                        {
+                            Base.Compute.RecordError($"Density is not set for material {material.Name}. Cannot evaluate mass based EPD.");
+                            return null;
+                        }
+                        if (material.Density == 0)
+                        {
+                            Base.Compute.RecordWarning($"Density of materials {material.Name} is 0 and will give no contribution for evaluating mass based EPD.");
+                        }
+                        quantityValue = materialVolume * material.Density;
+                        break;
+                    case QuantityType.Undefined:
+                    case QuantityType.Item:
+                    case QuantityType.Ampere:
+                    case QuantityType.VoltAmps:
+                    case QuantityType.VolumetricFlowRate:
+                    case QuantityType.Watt:
+                    default:
+                        Base.Compute.RecordError($"{epd.QuantityType} QuantityType is currently not supported.");
+                        return null;
+                }
+
+                materialResults.Add(new MaterialResult(material.Name, epd.Name, epd.EnvironmentalMetric.Where(x => x.Field == field).First().Phases, quantityValue * evaluationValue, field));
+            }
+
+            List<LifeCycleAssessmentPhases> evaluatedPhases = materialResults[0].Phases.ToList();
+            bool missMatchedPhases = false;
+
+            for (int i = 1; i < materialResults.Count; i++)
+            {
+                foreach (LifeCycleAssessmentPhases phase in materialResults[i].Phases)
+                {
+                    if (!evaluatedPhases.Contains(phase))
+                    {
+                        missMatchedPhases = true;
+                        evaluatedPhases.Add(phase);
+                    }
+                }
+            }
+            if (missMatchedPhases)
+                Base.Compute.RecordWarning("Missmatch in phases between different materialtypes on element. Please check the results.");
+
+            return new ElementResult(((IBHoMObject)elementM).BHoM_Guid, elementM.GetElementScope(), ObjectCategory.Undefined, evaluatedPhases, materialResults.Sum(x => x.Quantity), field, materialResults);
+
+        }
+
+        /***************************************************/
+ 
     }
 }
 
