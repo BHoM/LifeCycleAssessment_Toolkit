7456e221ee1386f57b0eb91096936d2a7d7077cc
Isak Naslund <Isak.Naslund@burohappold.com>
2022-11-07 13:30:16 +0100

    Add method for evaluating a volumetric material takeoff

---
+++ LifeCycleAssessment_Engine/Query/EvaluateMaterialTakeoff.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using BH.oM.Dimensional;
+using BH.oM.LifeCycleAssessment.MaterialFragments;
+using BH.oM.LifeCycleAssessment;
+using BH.oM.LifeCycleAssessment.Results;
+using BH.oM.Physical.Materials;
+using BH.oM.Quantities.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.Engine.LifeCycleAssessment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Evaluates the materials in the VolumetricMaterialTakeoff and returns a MaterialResult per material in the takeoff. Requires the materials in the Takeoff to have EPDs assigned. Please use the AssignTemplate methods before calling this method.")]
+        [Input("materialTakeoff", "THe volumetric material takeoff to evaluate.")]
+        [Input("phases", "Provide phases of life you wish to evaluate. Phases of life must be documented within EPDs for this method to work.")]
+        [Input("field", "Filter the provided EnvironmentalProductDeclaration by selecting one of the provided metrics for calculation. This method also accepts multiple fields simultaneously.")]
+        [Input("exactMatch", "If true, the evaluation method will force an exact LCA phase match to solve for.")]
+        [Output("", "")]
+        public static List<MaterialResult> EvaluateMaterialTakeoff(this VolumetricMaterialTakeoff materialTakeoff, List<LifeCycleAssessmentPhases> phases, EnvironmentalProductDeclarationField field = EnvironmentalProductDeclarationField.GlobalWarmingPotential, bool exactMatch = false)
+        {
+            if (materialTakeoff == null)
+                return new List<MaterialResult>();
+
+            List<MaterialResult> materialResults = new List<MaterialResult>();
+
+            for (int i = 0; i < materialTakeoff.Materials.Count; i++)
+            {
+                Material material = materialTakeoff.Materials[i];
+                EnvironmentalProductDeclaration epd = material.Properties.OfType<EnvironmentalProductDeclaration>().FirstOrDefault();
+
+                if (epd == null)
+                {
+                    Engine.Base.Compute.RecordError($"EPD not set to material {material.Name}. Unable to evaluate {field}.");
+                    return null;
+                }
+
+                double evaluationValue = epd.GetEvaluationValue(field, phases, exactMatch);
+                if (double.IsNaN(evaluationValue))
+                {
+                    Base.Compute.RecordError($"EPD {epd.Name} does not contain {field} for phases {string.Join(",", phases)}.");
+                    return null;
+                }
+
+                double quantityValue;
+                switch (epd.QuantityType)
+                {
+                    case QuantityType.Volume:
+                        quantityValue = materialTakeoff.Volumes[i];
+                        break;
+                    case QuantityType.Mass:
+                        if (double.IsNaN(material.Density))
+                        {
+                            Base.Compute.RecordError($"Density is not set for material {material.Name}. Cannot evaluate mass based EPD.");
+                            return null;
+                        }
+                        if (material.Density == 0)
+                        {
+                            Base.Compute.RecordWarning($"Density of materials {material.Name} is 0 and will give no contribution for evaluating mass based EPD.");
+                        }
+                        quantityValue = materialTakeoff.Volumes[i] * material.Density;
+                        break;
+                    case QuantityType.Length:
+                    case QuantityType.Area:
+                    case QuantityType.Undefined:
+                    case QuantityType.Item:
+                    case QuantityType.Ampere:
+                    case QuantityType.VoltAmps:
+                    case QuantityType.VolumetricFlowRate:
+                    case QuantityType.Watt:
+                    default:
+                        Base.Compute.RecordError($"{epd.QuantityType} QuantityType is currently not supported.");
+                        return null;
+                }
+
+                materialResults.Add(new MaterialResult(material.Name, epd.Name, epd.EnvironmentalMetric.Where(x => x.Field == field).First().Phases, quantityValue * evaluationValue, field));
+            }
+
+            return materialResults;
+        }
+
+        /***************************************************/
+    }
+}
