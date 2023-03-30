f7995ab1544e277347e7969485295361f1469788
Isak Naslund <Isak.Naslund@burohappold.com>
2023-03-30 09:54:39 +0200

    Adding create method descriptions

--- LifeCycleAssessment_Engine/Create/GlobalWarmingPotentialMetrics.cs
+++ LifeCycleAssessment_Engine/Create/GlobalWarmingPotentialMetrics.cs
@@ -36,9 +36,26 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("")]
-        [Input("", "")]
-        [Output("", "")]
+        [Description("Creates a GlobalWarmingPotentialMetrics to be applied as part of an EnvironmentalProductDeclaration. Value of A1toA3 will be computed based on provided A1, A2 and A3 values.")]
+        [InputFromProperty("a1")]
+        [InputFromProperty("a2")]
+        [InputFromProperty("a3")]
+        [InputFromProperty("a4")]
+        [InputFromProperty("a5")]
+        [InputFromProperty("b1")]
+        [InputFromProperty("b2")]
+        [InputFromProperty("b3")]
+        [InputFromProperty("b4")]
+        [InputFromProperty("b5")]
+        [InputFromProperty("b6")]
+        [InputFromProperty("b7")]
+        [InputFromProperty("c1")]
+        [InputFromProperty("c2")]
+        [InputFromProperty("c3")]
+        [InputFromProperty("c4")]
+        [InputFromProperty("d")]
+        [InputFromProperty("biogenicCarbon")]
+        [Output("gwpMetric", "Created GlobalWarmingPotentialMetrics.")]
         public static GlobalWarmingPotentialMetrics GlobalWarmingPotentialMetrics(
             double a1 = double.NaN,
             double a2 = double.NaN,
@@ -65,9 +82,24 @@ namespace BH.Engine.LifeCycleAssessment
 
         /***************************************************/
 
-        [Description("")]
-        [Input("", "")]
-        [Output("", "")]
+        [Description("Creates a GlobalWarmingPotentialMetrics to be applied as part of an EnvironmentalProductDeclaration. Create method to be used when no discrete values for A1, A2 and A3 are available, but only a total value for those 3 phases.")]
+        [InputFromProperty("a1toa3")]
+        [InputFromProperty("a4")]
+        [InputFromProperty("a5")]
+        [InputFromProperty("b1")]
+        [InputFromProperty("b2")]
+        [InputFromProperty("b3")]
+        [InputFromProperty("b4")]
+        [InputFromProperty("b5")]
+        [InputFromProperty("b6")]
+        [InputFromProperty("b7")]
+        [InputFromProperty("c1")]
+        [InputFromProperty("c2")]
+        [InputFromProperty("c3")]
+        [InputFromProperty("c4")]
+        [InputFromProperty("d")]
+        [InputFromProperty("biogenicCarbon")]
+        [Output("gwpMetric", "Created GlobalWarmingPotentialMetrics.")]
         public static GlobalWarmingPotentialMetrics GlobalWarmingPotentialMetrics(
                 double a1toa3 = double.NaN,
                 double a4 = double.NaN,
