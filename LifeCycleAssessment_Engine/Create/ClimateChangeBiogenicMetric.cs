d3845f030bb9209ab1c6c7d0e3a6c26780efda70
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-18 11:23:08 +0200

    Bulk addition of Create methods for metrics

--- LifeCycleAssessment_Engine/Create/ClimateChangeBiogenicMetric.cs
+++ LifeCycleAssessment_Engine/Create/ClimateChangeBiogenicMetric.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
  *
@@ -36,7 +36,7 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Creates a GlobalWarmingPotentialMetrics to be applied as part of an EnvironmentalProductDeclaration. Value of A1toA3 will be computed based on provided A1, A2 and A3 values.")]
+        [Description("Creates a ClimateChangeBiogenicMetric to be applied as part of an EnvironmentalProductDeclaration. Values of A1toA3, B1toB7 and C1toC4 will be computed as the sum of the relevant phases.")]
         [InputFromProperty("a1")]
         [InputFromProperty("a2")]
         [InputFromProperty("a3")]
@@ -54,9 +54,8 @@ namespace BH.Engine.LifeCycleAssessment
         [InputFromProperty("c3")]
         [InputFromProperty("c4")]
         [InputFromProperty("d")]
-        [InputFromProperty("biogenicCarbon")]
-        [Output("gwpMetric", "Created GlobalWarmingPotentialMetrics.")]
-        public static ClimateChangeTotalMetric ClimateChangeTotalMetric(
+        [Output("apMetric", "Created ClimateChangeBiogenicMetric.")]
+        public static ClimateChangeBiogenicMetric ClimateChangeBiogenicMetric(
             double a1 = double.NaN,
             double a2 = double.NaN,
             double a3 = double.NaN,
@@ -73,18 +72,17 @@ namespace BH.Engine.LifeCycleAssessment
             double c2 = double.NaN,
             double c3 = double.NaN,
             double c4 = double.NaN,
-            double d = double.NaN,
-            double biogenicCarbon = double.NaN)
+            double d = double.NaN)
         {
             double a1toa3 = a1 + a2 + a3;
             double b1tob7 = b1 + b2 + b3 + b4 + b5 + b6 + b7;
             double c1toc4 = c1 + c2 + c3 + c4;
-            return new ClimateChangeTotalMetric(a1, a2, a3, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d);
+            return new ClimateChangeBiogenicMetric(a1, a2, a3, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d);
         }
 
         /***************************************************/
 
-        [Description("Creates a GlobalWarmingPotentialMetrics to be applied as part of an EnvironmentalProductDeclaration. Create method to be used when no discrete values for A1, A2 and A3 are available, but only a total value for those 3 phases.")]
+        [Description("Creates a ClimateChangeBiogenicMetric to be applied as part of an EnvironmentalProductDeclaration. Create method to be used when no discrete values for A1, A2 and A3 are available, but only a total value for those 3 phases. Values of B1toB7 and C1toC4 will be computed as the sum of the relevant phases.")]
         [InputFromProperty("a1toa3", "A1toA3")]
         [InputFromProperty("a4")]
         [InputFromProperty("a5")]
@@ -100,9 +98,8 @@ namespace BH.Engine.LifeCycleAssessment
         [InputFromProperty("c3")]
         [InputFromProperty("c4")]
         [InputFromProperty("d")]
-        [InputFromProperty("biogenicCarbon")]
-        [Output("gwpMetric", "Created GlobalWarmingPotentialMetrics.")]
-        public static ClimateChangeTotalMetric ClimateChangeTotalMetric(
+        [Output("apMetric", "Created ClimateChangeBiogenicMetric.")]
+        public static ClimateChangeBiogenicMetric ClimateChangeBiogenicMetric(
                 double a1toa3 = double.NaN,
                 double a4 = double.NaN,
                 double a5 = double.NaN,
@@ -117,13 +114,33 @@ namespace BH.Engine.LifeCycleAssessment
                 double c2 = double.NaN,
                 double c3 = double.NaN,
                 double c4 = double.NaN,
-                double d = double.NaN,
-                double biogenicCarbon = double.NaN)
+                double d = double.NaN)
         {
             double b1tob7 = b1 + b2 + b3 + b4 + b5 + b6 + b7;
             double c1toc4 = c1 + c2 + c3 + c4;
-            return new ClimateChangeTotalMetric(double.NaN, double.NaN, double.NaN, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d);
+            return new ClimateChangeBiogenicMetric(double.NaN, double.NaN, double.NaN, a1toa3, a4, a5, b1, b2, b3, b4, b5, b6, b7, b1tob7, c1, c2, c3, c4, c1toc4, d);
+        }
 
+        /***************************************************/
+
+        [Description("Creates a ClimateChangeBiogenicMetric to be applied as part of an EnvironmentalProductDeclaration. Create method to be used when no discrete values for the phases in the Product stage (A1 - A3), use stage (B1-B7) or end of life stage (C1-C4) is given, but only the total value for the phases in those stages are available.")]
+        [InputFromProperty("a1toa3", "A1toA3")]
+        [InputFromProperty("a4")]
+        [InputFromProperty("a5")]
+        [InputFromProperty("b1tob7", "B1toB7")]
+        [InputFromProperty("c1toc4", "C1toC4")]
+        [InputFromProperty("d")]
+        [Output("apMetric", "Created ClimateChangeBiogenicMetric.")]
+        public static ClimateChangeBiogenicMetric ClimateChangeBiogenicMetric(
+                double a1toa3 = double.NaN,
+                double a4 = double.NaN,
+                double a5 = double.NaN,
+                double b1tob7 = double.NaN,
+                double c1toc4 = double.NaN,
+                double d = double.NaN)
+        {
+
+            return new ClimateChangeBiogenicMetric(double.NaN, double.NaN, double.NaN, a1toa3, a4, a5, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, double.NaN, b1tob7, double.NaN, double.NaN, double.NaN, double.NaN, c1toc4, d);
         }
 
         /***************************************************/
