97d2d76fc5eacc79c420c420773d4a5720293c52
IsakNaslundBh <isak.naslund@burohappold.com>
2024-12-18 12:05:26 +0100

    Update QuantityType.cs

--- LifeCycleAssessment_Engine/Query/QuantityType.cs
+++ LifeCycleAssessment_Engine/Query/QuantityType.cs
@@ -28,6 +28,7 @@ using BH.oM.Dimensional;
 using System.Collections.Generic;
 using BH.Engine.Matter;
 using System.Linq;
+using BH.oM.LifeCycleAssessment.MaterialFragments.Transport;
 
 namespace BH.Engine.LifeCycleAssessment
 {
@@ -50,8 +51,27 @@ namespace BH.Engine.LifeCycleAssessment
                 return new List<QuantityType> { oM.LifeCycleAssessment.QuantityType.Undefined };
             }
 
-            return elementM.ElementEnvironmentalMetricProviders().Select(x => x == null ? oM.LifeCycleAssessment.QuantityType.Undefined : x.QuantityType).ToList();
+            List<QuantityType> quantityTypes = new List<QuantityType>();
 
+            foreach (IEnvironmentalMetricsProvider metricsProvider in elementM.ElementEnvironmentalMetricProviders())
+            {
+                if (metricsProvider is IBasicEnvironmentalMetricsProvider basicProvider)
+                    quantityTypes.Add(basicProvider.QuantityType);
+                else if (metricsProvider is ITransportFactors)
+                    quantityTypes.Add(QuantityType.Mass);
+                else if (metricsProvider is CombinedLifeCycleAssessmentFactors combinedFactors)
+                {
+                    if (combinedFactors.EnvironmentalProductDeclaration != null)
+                        quantityTypes.Add(combinedFactors.EnvironmentalProductDeclaration.QuantityType);
+                    if (combinedFactors.TransportFactors != null)
+                        quantityTypes.Add(QuantityType.Mass);
+                }
+                else
+                {
+                    BH.Engine.Base.Compute.RecordWarning($"Unable to get quantity type for metric providers of type {metricsProvider.GetType().Name}");
+                }
+            }
+            return quantityTypes;
 
         }
 
