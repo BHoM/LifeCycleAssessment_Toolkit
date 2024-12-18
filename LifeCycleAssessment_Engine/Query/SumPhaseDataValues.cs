528398f621b60b0f4d57777f671b95ac5a45c59a
IsakNaslundBh <isak.naslund@burohappold.com>
2024-12-18 12:04:20 +0100

    Add toggle for SumPhaseDataValues to allow for ignoring of NaN vlaues where there is a non NaN value on the same phase

--- LifeCycleAssessment_Engine/Query/SumPhaseDataValues.cs
+++ LifeCycleAssessment_Engine/Query/SumPhaseDataValues.cs
@@ -39,10 +39,12 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Public Methods - Interface                ****/
         /***************************************************/
 
+        [PreviousVersion("8.0", "BH.Engine.LifeCycleAssessment.Query.SumPhaseDataValues(System.Collections.Generic.IReadOnlyList<BH.oM.LifeCycleAssessment.ILifeCycleAssessmentPhaseData>)")]
         [Description("Gets a list of doubles corresponding to the sum of values for each property of the provided ILifeCycleAssessmentPhaseData, i.e. the first value will be the sum of A1 for all the provided ILifeCycleAssessmentPhaseDatas.")]
         [Input("phaseData", "List of ILifeCycleAssessmentPhaseData to get the sum data from.")]
+        [Input("treatNanAsZero", "If true NaN values will be treated as zero if there is a non-NaN value for the same phase present, in other words, 1 + NaN will be treated as 1. If false, no check for NaN values will be done, and if a NaN value is present for a particular phase, the whole sum will be treated as NaN.")]
         [Output("values", "The values of the summed up material results as list of doubles where each item in the list corresponds to the sum for a particular phase, i.e. first item in the list will be the sum of all A1s in the provided phase data.")]
-        public static List<double> SumPhaseDataValues(this IReadOnlyList<ILifeCycleAssessmentPhaseData> phaseData)
+        public static List<double> SumPhaseDataValues(this IReadOnlyList<ILifeCycleAssessmentPhaseData> phaseData, bool treatNanAsZero = false)
         {
             if(phaseData.IsNullOrEmpty())
                 return new List<double>();
@@ -70,10 +72,29 @@ namespace BH.Engine.LifeCycleAssessment
                 List<double> current = phaseData[i].IPhaseDataValues();
 
                 //Loop through all the phase data and add to the sum
-                for (int j = 0; j < sum.Count; j++)
+                if (treatNanAsZero)
                 {
-                    sum[j] += current[j];
+                    for (int j = 0; j < sum.Count; j++)
+                    {
+                        double val = current[j];
+                        if (!double.IsNaN(val))
+                        {
+                            if (double.IsNaN(sum[i]))
+                                sum[i] = val;
+                            else
+                                sum[i] += val;
+                        }
+                    }
                 }
+                else
+                {
+                    for (int j = 0; j < sum.Count; j++)
+                    {
+                        sum[j] += current[j];
+                    }
+                }
+
+  
             }
 
             return sum;
--- LifeCycleAssessment_Engine/Query/TotalMaterialBreakdown.cs
+++ LifeCycleAssessment_Engine/Query/TotalMaterialBreakdown.cs
@@ -41,23 +41,27 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Public Methods                            ****/
         /***************************************************/
 
+        [PreviousVersion("8.0", "BH.Engine.LifeCycleAssessment.Query.TotalMaterialBreakdown(System.Collections.Generic.IEnumerable<BH.oM.LifeCycleAssessment.Results.IElementResult<BH.oM.LifeCycleAssessment.Results.MaterialResult>>)")]
         [Description("Gets total MaterialResults for all provided element results grouped by MaterialName, EPDName and Metric, and returns a single MaterialResult for each group containing the total evaluated.")]
         [Input("elementResults", "The element results to extract the material breakdown from.")]
+        [Input("treatNanAsZero", "If true NaN values will be treated as zero if there is a non-NaN value for the same phase present, in other words, 1 + NaN will be treated as 1. If false, no check for NaN values will be done, and if a NaN value is present for a particular phase, the whole sum will be treated as NaN.")]
         [Output("materialResults", "Material results with the total quantity per materal type.")]
-        public static List<MaterialResult> TotalMaterialBreakdown(this IEnumerable<IElementResult<MaterialResult>> elementResults)
+        public static List<MaterialResult> TotalMaterialBreakdown(this IEnumerable<IElementResult<MaterialResult>> elementResults, bool treatNanAsZero = false)
         {
             if (elementResults == null || !elementResults.Any())
                 return new List<MaterialResult>();
 
-            return elementResults.SelectMany(x => x.MaterialResults).TotalMaterialBreakdown();
+            return elementResults.SelectMany(x => x.MaterialResults).TotalMaterialBreakdown(treatNanAsZero);
         }
 
         /***************************************************/
 
+        [PreviousVersion("8.0", "BH.Engine.LifeCycleAssessment.Query.TotalMaterialBreakdown(System.Collections.Generic.IEnumerable<BH.oM.LifeCycleAssessment.Results.MaterialResult>)")]
         [Description("Gets total MaterialResults from list of individual material results grouped by MaterialName, EPDName and Type, and returns a single MaterialResult for each group containing the total evaluated.")]
         [Input("materialResults", "The individual MaterialResult results to extract the total from.")]
+        [Input("treatNanAsZero", "If true NaN values will be treated as zero if there is a non-NaN value for the same phase present, in other words, 1 + NaN will be treated as 1. If false, no check for NaN values will be done, and if a NaN value is present for a particular phase, the whole sum will be treated as NaN.")]
         [Output("materialResults", "Material results with the total quantity per materal type.")]
-        public static List<MaterialResult> TotalMaterialBreakdown(this IEnumerable<MaterialResult> materialResults)
+        public static List<MaterialResult> TotalMaterialBreakdown(this IEnumerable<MaterialResult> materialResults, bool treatNanAsZero = false)
         {
             if (materialResults == null || !materialResults.Any())
                 return new List<MaterialResult>();
@@ -66,7 +70,7 @@ namespace BH.Engine.LifeCycleAssessment
 
             foreach (var group in materialResults.GroupBy(x => x.GetType()))
             {
-                breakDown.AddRange(MaterialBreakdown(group));
+                breakDown.AddRange(MaterialBreakdown(group, treatNanAsZero));
             }
 
             return breakDown;
@@ -79,7 +83,7 @@ namespace BH.Engine.LifeCycleAssessment
         [Description("Gets total MaterialResults from list of individual material results grouped by MaterialName and EPDName, and returns a single MaterialResult for each group containing the total evaluated for each phase.")]
         [Input("materialResults", "The individual MaterialResult results to extract the total from All assumed to be of the same type.")]
         [Output("materialResults", "Material results with the total quantity per materal type.")]
-        private static List<MaterialResult> MaterialBreakdown(IEnumerable<MaterialResult> materialResults)
+        private static List<MaterialResult> MaterialBreakdown(IEnumerable<MaterialResult> materialResults, bool treatNanAsZero = false)
         {
             List<MaterialResult> breakDown = new List<MaterialResult>();
             ////Get current type being evaluated
@@ -88,7 +92,7 @@ namespace BH.Engine.LifeCycleAssessment
             foreach (var group in materialResults.GroupBy(x => new { x.MaterialName, x.EnvironmentalProductDeclarationName }))
             {
                 //Compute the total breakdown as sum value for each phase
-                List<double> sumValues = group.ToList().SumPhaseDataValues();
+                List<double> sumValues = group.ToList().SumPhaseDataValues(treatNanAsZero);
                 //Call create and add to list
                 breakDown.Add(Create.MaterialResult(type, group.Key.MaterialName, group.Key.EnvironmentalProductDeclarationName, sumValues));
                
