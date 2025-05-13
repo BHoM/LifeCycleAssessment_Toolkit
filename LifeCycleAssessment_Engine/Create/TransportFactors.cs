94f2246b3c765ddf9defabf1f5278af8b53e5a21
Isak Näslund <isak.naslund@burohappold.com>
2025-05-13 08:41:38 +0100

    remove unit input for transport factors

--- LifeCycleAssessment_Engine/Create/TransportFactors.cs
+++ LifeCycleAssessment_Engine/Create/TransportFactors.cs
@@ -46,12 +46,12 @@ namespace BH.Engine.LifeCycleAssessment
         [Input("lengthUnits", "Units for distance traveled. Supported units are metres, kilometres and miles (m, km and mi). Distance set on the created object will be in the SI unit meters. Used in the methodology is set to DistanceTransportMode.")]
         [Input("customFactor", "Custom factor for explicitly setting the climate change transport emissions. Value should be per mass (kg). Will create new metrics for ClimateChangeFossil, ClimateChangeTotal and ClimateCHangeTotalNoBiogenic and set the A4 value to this value, and return a custom full transport scenario with the metrics set.")]
         [Output("transportFactors", "The created transport factors, type depending on provided methodology.")]
-        public static ITransportFactors TransportFactors(TransportMethodology methodology, FullTransportScenario typicalScenario = null, VehicleEmissions vehicleEmissions = null, double distanceTraveled = double.NaN, string lengthUnits = "m", double customFactor = double.NaN)
+        public static ITransportFactors TransportFactors(TransportMethodology methodology, FullTransportScenario typicalScenario = null, VehicleEmissions vehicleEmissions = null, double distanceTraveled = double.NaN, double customFactor = double.NaN)
         {
             switch (methodology)
             {
                 case TransportMethodology.DistanceTransportMode:
-                    return DistanceTransportMode(vehicleEmissions, distanceTraveled, lengthUnits);
+                    return DistanceTransportMode(vehicleEmissions, distanceTraveled);
                 case TransportMethodology.TypicalTransportScenario:
                     if (typicalScenario == null)
                         Base.Compute.RecordError("Methodology is set to typical transport scenario but the provided typical transport scenario is null");
@@ -73,7 +73,7 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Private Methods                           ****/
         /***************************************************/
 
-        private static DistanceTransportModeScenario DistanceTransportMode(VehicleEmissions vehicleEmissions, double distanceTraveled, string lengthUnits)
+        private static DistanceTransportModeScenario DistanceTransportMode(VehicleEmissions vehicleEmissions, double distanceTraveled)
         {
             if (vehicleEmissions == null)
             {
@@ -87,24 +87,24 @@ namespace BH.Engine.LifeCycleAssessment
                 return null;
             }
 
-            lengthUnits = lengthUnits.ToLower().Replace(" ", "");
+            //lengthUnits = lengthUnits.ToLower().Replace(" ", "");
 
-            double scaleFactor = 1;
-            if (lengthUnits == "m" || lengthUnits == "metre" || lengthUnits == "metres" || lengthUnits == "meter" || lengthUnits == "meters")
-                scaleFactor = 1;
-            else if (lengthUnits == "km" || lengthUnits == "kilometer" || lengthUnits == "kilometers" || lengthUnits == "kilometre" || lengthUnits == "kilometres")
-                scaleFactor = 1000;
-            else if (lengthUnits == "mi" || lengthUnits == "mile" || lengthUnits == "miles")
-                scaleFactor = 1609.344;
-            else
-            {
-                Base.Compute.RecordError("length unit is unkown. Supported values are 'm', 'meter', 'metre', 'metres' and 'meters' for metres, 'km', 'kilometer', 'kilometre', 'kilometres' and 'kilometers' for kilometres and 'mi','mile' and 'miles' for miles.");
-                return null;
-            }
+            //double scaleFactor = 1;
+            //if (lengthUnits == "m" || lengthUnits == "metre" || lengthUnits == "metres" || lengthUnits == "meter" || lengthUnits == "meters")
+            //    scaleFactor = 1;
+            //else if (lengthUnits == "km" || lengthUnits == "kilometer" || lengthUnits == "kilometers" || lengthUnits == "kilometre" || lengthUnits == "kilometres")
+            //    scaleFactor = 1000;
+            //else if (lengthUnits == "mi" || lengthUnits == "mile" || lengthUnits == "miles")
+            //    scaleFactor = 1609.344;
+            //else
+            //{
+            //    Base.Compute.RecordError("length unit is unkown. Supported values are 'm', 'meter', 'metre', 'metres' and 'meters' for metres, 'km', 'kilometer', 'kilometre', 'kilometres' and 'kilometers' for kilometres and 'mi','mile' and 'miles' for miles.");
+            //    return null;
+            //}
 
             return new DistanceTransportModeScenario
             {
-                SingleTransportModeImpacts = new List<SingleTransportModeImpact> { new SingleTransportModeImpact { VehicleEmissions = vehicleEmissions, DistanceTraveled = distanceTraveled * scaleFactor } }
+                SingleTransportModeImpacts = new List<SingleTransportModeImpact> { new SingleTransportModeImpact { VehicleEmissions = vehicleEmissions, DistanceTraveled = distanceTraveled } }
             };
 
         }
