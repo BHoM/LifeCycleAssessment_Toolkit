764e87c76ec0b26e21c616b7b9f8f0d31621dbd5
Isak Näslund <isak.naslund@burohappold.com>
2025-05-16 10:39:34 +0100

    Add tests for MetricType

---
+++ .ci/unit-tests/LifeCycleAssessment_Engine_Tests/MetricType.cs
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoBogus;
+using BH.oM.LifeCycleAssessment.MaterialFragments;
+using BH.oM.LifeCycleAssessment.Results;
+using BH.Engine.LifeCycleAssessment;
+using FluentAssertions;
+using System.Reflection;
+using BH.oM.LifeCycleAssessment;
+using BH.oM.Physical.Materials;
+
+namespace BH.Tests.Engine.LifeCycleAssessment
+{
+    public class MetricTypeTests
+    {
+        /***************************************************/
+        /****   Public Methods                          ****/
+        /***************************************************/
+
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.DummyMetrics), new object[] { 1, 0, false })]
+        public void MetricTypeNotUndefined(IEnvironmentalMetricFactors metricFactors)
+        {
+            MetricType metricType = metricFactors.IMetricType();
+            metricType.Should().NotBe(MetricType.Undefined);
+        }
+
+        /***************************************************/
+
+
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.DummyMetricsList), new object[] { 1, 0, false })]
+        public void MetricTypesAllUnique(List<IEnvironmentalMetricFactors> metricFactors)
+        {
+            List<MetricType> metricTypes = metricFactors.Select(x => x.IMetricType()).ToList();
+            metricTypes.Should().OnlyHaveUniqueItems();
+        }
+
+        /***************************************************/
+
+    }
+}
+
+
