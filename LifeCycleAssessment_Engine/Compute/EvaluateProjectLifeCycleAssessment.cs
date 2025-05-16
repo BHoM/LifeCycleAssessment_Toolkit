c4afac87709b95faf4de14eed8a95357b3c7f742
Isak Näslund <isak.naslund@burohappold.com>
2025-05-16 10:52:30 +0100

    Fix basic compliance

--- LifeCycleAssessment_Engine/Compute/EvaluateProjectLifeCycleAssessment.cs
+++ LifeCycleAssessment_Engine/Compute/EvaluateProjectLifeCycleAssessment.cs
@@ -1,72 +1,72 @@
-///*
-// * This file is part of the Buildings and Habitats object Model (BHoM)
-// * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
-// *
-// * Each contributor holds copyright over their respective contributions.
-// * The project versioning (Git) records all such contribution source information.
-// *                                           
-// *                                                                              
-// * The BHoM is free software: you can redistribute it and/or modify         
-// * it under the terms of the GNU Lesser General Public License as published by  
-// * the Free Software Foundation, either version 3.0 of the License, or          
-// * (at your option) any later version.                                          
-// *                                                                              
-// * The BHoM is distributed in the hope that it will be useful,              
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of               
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
-// * GNU Lesser General Public License for more details.                          
-// *                                                                            
-// * You should have received a copy of the GNU Lesser General Public License     
-// * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
-// */
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
 
-//using BH.oM.Dimensional;
-//using BH.oM.LifeCycleAssessment;
-//using BH.oM.LifeCycleAssessment.Results;
-//using BH.oM.Base.Attributes;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Linq;
-//using System;
-//using BH.oM.Physical.Materials;
+using BH.oM.Dimensional;
+using BH.oM.LifeCycleAssessment;
+using BH.oM.LifeCycleAssessment.Results;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System;
+using BH.oM.Physical.Materials;
 
-//namespace BH.Engine.LifeCycleAssessment
-//{
-//    public static partial class Compute
-//    {
-//        /***************************************************/
-//        /****   Public Methods                          ****/
-//        /***************************************************/
+namespace BH.Engine.LifeCycleAssessment
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /****   Public Methods                          ****/
+        /***************************************************/
 
-//        [Description("This method calculates the total field quantity specified for an entire project or a collection of elements. Metadata included on the ProjectLCA object is not accounted for within this calculation.")]
-//        [Input("projectLCA", "Project LCA can be used to collect all objects used in an evaluation along with the project's specific metatdata for tracking within a database.")]
-//        [Input("field", "Filter the provided EnvironmentalProductDeclaration by selecting one of the provided metrics for calculation. This method also accepts multiple fields simultaneously.")]
-//        [Output("quantity", "The total quantity of the specified metric.")]
-//        public static double EvaluateProjectLifeCycleAssessment(ProjectLifeCycleAssessment projectLCA, MetricType field, List<Material> templateMaterials = null, bool prioritiseTemplate = true)
-//        {
-//            if(projectLCA == null)
-//            {
-//                Base.Compute.RecordError("No Project LCA was provided.");
-//                return double.NaN;
-//            }
-            
-//            List<IElementM> elements = projectLCA.Elements;
+        [Description("This method calculates the total field quantity specified for an entire project or a collection of elements. Metadata included on the ProjectLCA object is not accounted for within this calculation.")]
+        [Input("projectLCA", "Project LCA can be used to collect all objects used in an evaluation along with the project's specific metatdata for tracking within a database.")]
+        [Input("field", "Filter the provided EnvironmentalProductDeclaration by selecting one of the provided metrics for calculation. This method also accepts multiple fields simultaneously.")]
+        [Output("quantity", "The total quantity of the specified metric.")]
+        public static double EvaluateProjectLifeCycleAssessment(ProjectLifeCycleAssessment projectLCA, MetricType field, List<Material> templateMaterials = null, bool prioritiseTemplate = true)
+        {
+            if (projectLCA == null)
+            {
+                Base.Compute.RecordError("No Project LCA was provided.");
+                return double.NaN;
+            }
 
-//            if (elements.Count <=0)
-//            {
-//                Base.Compute.RecordError("No elements were found in the Project LCA.");
-//                return double.NaN;
-//            }
+            List<IElementM> elements = projectLCA.Elements;
 
-//            var results = projectLCA.Elements.SelectMany(x => Query.EnvironmentalResults(x, templateMaterials, prioritiseTemplate, new List<MetricType> { field })).ToList();
+            if (elements.Count <= 0)
+            {
+                Base.Compute.RecordError("No elements were found in the Project LCA.");
+                return double.NaN;
+            }
 
-//            return results.Sum(x => x.Total());
-//        }
-//        /***************************************************/
+            var results = projectLCA.Elements.SelectMany(x => Query.EnvironmentalResults(x, templateMaterials, prioritiseTemplate, new List<MetricType> { field })).ToList();
+
+            return results.Sum(x => x.Total());
+        }
+        /***************************************************/
 
 
-//    }
-//}
+    }
+}
 
 
 
--- LifeCycleAssessment_Engine/Query/ValidateConfig.cs
+++ LifeCycleAssessment_Engine/Query/ValidateConfig.cs
@@ -1,4 +1,26 @@
-﻿using BH.oM.LifeCycleAssessment.Configs;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.LifeCycleAssessment.Configs;
 using BH.oM.LifeCycleAssessment.MaterialFragments;
 using BH.oM.LifeCycleAssessment;
 using System;
@@ -19,7 +41,7 @@ namespace BH.Engine.LifeCycleAssessment
         [Input("config", "The config to evaluate against the provider.")]
         [Input("factorsProvider", "The provider to validate the config against.")]
         [Output("isValid", "Returns true if the config is valid to be used with the provider - false if not.")]
-        public static bool IValidateConfig(IEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
+        public static bool IValidateConfig(this IEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
         {
             if (config == null) //Null config is valid, as default case of evaluation is assumed for provided null config.
                 return true;
@@ -33,7 +55,7 @@ namespace BH.Engine.LifeCycleAssessment
         [Input("config", "The config to evaluate against the provider.")]
         [Input("factorsProvider", "The provider to validate the config against.")]
         [Output("isValid", "Returns true if the config is valid to be used with the provider - false if not.")]
-        public static bool ValidateConfig(IStructEEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
+        public static bool ValidateConfig(this IStructEEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
         {
             if(config == null) 
                 return true;
@@ -53,7 +75,7 @@ namespace BH.Engine.LifeCycleAssessment
         /**** Private Methods - Validation - Fallback   ****/
         /***************************************************/
 
-        private static bool ValidateConfig(IEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
+        private static bool ValidateConfig(this IEvaluationConfig config, IEnvironmentalFactorsProvider factorsProvider)
         {
             return true;    //Default to true for fallback
         }
